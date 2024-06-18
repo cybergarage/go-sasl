0fa355447f8e436b36f7fea84a619747eca933d1
Satoshi Konno <skonno@cybergarage.org>
2024-06-18 21:57:50 +0900

    Update Property interface

--- sasl/property.go
+++ sasl/property.go
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package scram
+package sasl
 
-// Property represents a SCRAM property.
+// Property represents a SASL property.
 type Property interface {
 	// Name returns the property name.
 	Name() string
