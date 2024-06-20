f6febd6211725679e82cc12ba47f3f1839678de2
Satoshi Konno <skonno@cybergarage.org>
2024-06-21 01:10:45 +0900

    Update TestSCRAMMessage()

--- sasl/scram/message_test.go
+++ sasl/scram/message_test.go
@@ -20,8 +20,12 @@ import (
 
 func TestSCRAMMessage(t *testing.T) {
 	type expected struct {
-		userName       string
-		randomSequence string
+		userName           string
+		randomSequence     string
+		salt               string
+		iterationCount     string
+		channelBindingData string
+		clientProof        string
 	}
 
 	tests := []struct {
@@ -29,15 +33,42 @@ func TestSCRAMMessage(t *testing.T) {
 		hasHeader  bool
 		expected   expected
 	}{
-
+		// RFC 5802 - Salted Challenge Response Authentication Mechanism (SCRAM) SASL and GSS-API Mechanisms
+		// 5. SCRAM Authentication Exchange
 		{
-			// RFC 5802 - Salted Challenge Response Authentication Mechanism (SCRAM) SASL and GSS-API Mechanisms
-			// 5. SCRAM Authentication Exchange
 			messageStr: "n,,n=user,r=fyko+d2lbbFgONRv9qkxdawL",
 			hasHeader:  true,
 			expected: expected{
-				userName:       "user",
-				randomSequence: "fyko+d2lbbFgONRv9qkxdawL",
+				userName:           "user",
+				randomSequence:     "fyko+d2lbbFgONRv9qkxdawL",
+				salt:               "",
+				iterationCount:     "",
+				channelBindingData: "",
+				clientProof:        "",
+			},
+		},
+		{
+			messageStr: "r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,s=QSXCR+Q6sek8bf92,i=4096",
+			hasHeader:  false,
+			expected: expected{
+				userName:           "",
+				randomSequence:     "fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j",
+				salt:               "QSXCR+Q6sek8bf92",
+				iterationCount:     "4096",
+				channelBindingData: "",
+				clientProof:        "",
+			},
+		},
+		{
+			messageStr: "c=biws,r=fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j,p=v0X8v3Bz2T0CJGbJQyF0X+HI4Ts=",
+			hasHeader:  false,
+			expected: expected{
+				userName:           "",
+				randomSequence:     "fyko+d2lbbFgONRv9qkxdawL3rfcNHYJY1ZVvWVs7j",
+				salt:               "",
+				iterationCount:     "",
+				channelBindingData: "biws",
+				clientProof:        "v0X8v3Bz2T0CJGbJQyF0X+HI4Ts=",
 			},
 		},
 	}
@@ -67,6 +98,34 @@ func TestSCRAMMessage(t *testing.T) {
 			v, ok := msg.RandomSequence()
 			if !ok || v != test.expected.randomSequence {
 				t.Errorf("randomSequence = %s, want %s", v, test.expected.randomSequence)
+			}
+		}
+
+		if 0 < len(test.expected.salt) {
+			v, ok := msg.Salt()
+			if !ok || v != test.expected.salt {
+				t.Errorf("salt = %s, want %s", v, test.expected.salt)
+			}
+		}
+
+		if 0 < len(test.expected.iterationCount) {
+			v, ok := msg.IterationCount()
+			if !ok || v != test.expected.iterationCount {
+				t.Errorf("iterationCount = %s, want %s", v, test.expected.iterationCount)
+			}
+		}
+
+		if 0 < len(test.expected.channelBindingData) {
+			v, ok := msg.ChannelBindingData()
+			if !ok || v != test.expected.channelBindingData {
+				t.Errorf("channelBindingData = %s, want %s", v, test.expected.channelBindingData)
+			}
+		}
+
+		if 0 < len(test.expected.clientProof) {
+			v, ok := msg.ClientProof()
+			if !ok || v != test.expected.clientProof {
+				t.Errorf("clientProof = %s, want %s", v, test.expected.clientProof)
 			}
 		}
 	}
