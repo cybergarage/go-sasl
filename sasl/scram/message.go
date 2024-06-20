bc2a39506248df9219827345e9a172cd64a4fb86
Satoshi Konno <skonno@cybergarage.org>
2024-06-20 23:56:44 +0900

    Update Message::ParseStrings()

--- sasl/scram/message.go
+++ sasl/scram/message.go
@@ -14,17 +14,64 @@
 
 package scram
 
+import (
+	"strings"
+
+	"github.com/cybergarage/go-sasl/sasl/gss"
+)
+
 // Message represents a SCRAM message.
 type Message struct {
+	*gss.Header
+	AttributeMap
 }
 
 // NewMessage returns a new Message.
 func NewMessage() *Message {
-	msg := &Message{}
+	msg := &Message{
+		Header:       nil,
+		AttributeMap: NewAttributeMap(),
+	}
 	return msg
 }
 
-// ParseString parses the specified data.
-func (msg *Message) ParseString(data string) error {
+// ParseString parses the specified string.
+func (msg *Message) ParseString(str string) error {
+	return msg.ParseStrings(strings.Split(str, ","))
+}
+
+// ParseStrings parses the specified property strings.
+func (msg *Message) ParseStrings(props []string) error {
+	var err error
+	msg.Header, err = gss.NewHeaderFromStrings(props)
+	if err != nil {
+		return err
+	}
+	scramProps := props[gss.GS2PropertyMaxCount:]
+	if !msg.Header.HasStdFlag() {
+		scramProps = props[(gss.GS2PropertyMaxCount - 1):]
+	}
+	for _, scramProp := range scramProps {
+		scramProps := strings.Split(scramProp, "=")
+		if len(scramProps) < 2 {
+			return newErrInvalidAttribute(scramProp)
+		}
+		attrName := scramProps[0]
+		switch scramProps[0] {
+		case UserName,
+			FutureExtensibility,
+			RandomSequence,
+			ChannelBindingData,
+			Salt,
+			IterationCount,
+			ClientProof,
+			ServerSignature,
+			Error:
+			prop := NewAttribute(attrName, scramProps[1])
+			msg.AttributeMap[attrName] = prop
+		default:
+			return newErrInvalidAttribute(scramProp)
+		}
+	}
 	return nil
 }
