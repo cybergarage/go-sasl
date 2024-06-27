75acd8a1d850b14b88ae12421845619932dde1b7
Satoshi Konno <skonno@cybergarage.org>
2024-06-28 00:40:32 +0900

    Add scram.NewRandomSequence()

---
+++ sasl/scram/rand.go
@@ -0,0 +1,39 @@
+// Copyright (C) 2024 The go-sasl Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package scram
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+const (
+	randomSequenceLength = 32
+)
+
+// NewRandomSequence returns a new random sequence.
+func NewRandomSequence() (string, error) {
+	return NewRandomSequenceWith(randomSequenceLength)
+}
+
+// RandomSequence represents a random sequence with the specified length.
+func NewRandomSequenceWith(length int) (string, error) {
+	randomBytes := make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(randomBytes), nil
+}
