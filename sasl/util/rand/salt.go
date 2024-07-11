00d379cd9dca2dd685c8d7ab2734b32e918902bf
Satoshi Konno <skonno@cybergarage.org>
2024-07-11 23:34:18 +0900

    Add rand.NewSalt()

---
+++ sasl/util/rand/salt.go
@@ -0,0 +1,33 @@
+// Copyright (C) 2024 The go-sasl Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package rand
+
+import (
+	"crypto/rand"
+	"encoding/base64"
+)
+
+// Salt represents a random salt.
+type Salt string
+
+// NewSalt creates a new random salt.
+func NewSalt(length int) (string, error) {
+	randomBytes := make([]byte, length)
+	_, err := rand.Read(randomBytes)
+	if err != nil {
+		return "", err
+	}
+	return base64.StdEncoding.EncodeToString(randomBytes), nil
+}
