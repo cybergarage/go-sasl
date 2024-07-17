12943a88b68c8e9fe850918cfb10e6dd5e83f180
Satoshi Konno <skonno@cybergarage.org>
2024-07-17 23:43:51 +0900

    Fix gocritic warnings

--- sasl/pkcs/pbkdf2.go
+++ sasl/pkcs/pbkdf2.go
@@ -17,7 +17,7 @@ package pkcs
 // RFC 2898 - PKCS #5: Password-Based Cryptography Specification Version 2.0
 // https://datatracker.ietf.org/doc/html/rfc2898
 // 5.2 PBKDF2.
-func PBKDF2(P []byte, S []byte, c int, dkLen int) []byte {
+func PBKDF2(p []byte, s []byte, c int, dkLen int) []byte {
 	// 5.2 PBKDF2
 	// https://datatracker.ietf.org/doc/html/rfc2898#section-5.2
 	// PBKDF2 (P, S, c, dkLen)
