bd1d0157c0534d7783893d51b407e33c5bf416c7
Satoshi Konno <skonno@cybergarage.org>
2024-07-27 21:04:16 +0900

    Update scram.AttributeMap::SetAttribute() and String()

--- sasl/scram/attribute_map.go
+++ sasl/scram/attribute_map.go
@@ -16,7 +16,6 @@ package scram
 
 import (
 	"encoding/base64"
-	"sort"
 	"strconv"
 	"strings"
 
@@ -25,18 +24,20 @@ import (
 
 // AttributeMap represents a SCRAM attribute map.
 type AttributeMap struct {
+	keys  []string
 	attrs map[string]Attribute
 }
 
 // NewAttributeMap returns a new SCRAM attribute map.
 func NewAttributeMap() AttributeMap {
 	return AttributeMap{
+		keys:  make([]string, 0),
 		attrs: make(map[string]Attribute),
 	}
 }
 
 // Attribute returns an attribute from the map.
-func (m AttributeMap) Attribute(name string) (string, bool) {
+func (m *AttributeMap) Attribute(name string) (string, bool) {
 	prop := m.attrs[name]
 	if prop == nil {
 		return "", false
@@ -45,7 +46,7 @@ func (m AttributeMap) Attribute(name string) (string, bool) {
 }
 
 // DecodeAttribute returns a base64 decoded attribute from the map.
-func (m AttributeMap) DecodeAttribute(name string) ([]byte, bool) {
+func (m *AttributeMap) DecodeAttribute(name string) ([]byte, bool) {
 	v, ok := m.Attribute(name)
 	if !ok {
 		return nil, false
@@ -58,12 +59,12 @@ func (m AttributeMap) DecodeAttribute(name string) ([]byte, bool) {
 }
 
 // AuthorizationID returns the authorization ID attribute from the map.
-func (m AttributeMap) AuthorizationID() (string, bool) {
+func (m *AttributeMap) AuthorizationID() (string, bool) {
 	return m.Attribute(AuthorizationIDAttr)
 }
 
 // Username returns the user name attribute from the map.
-func (m AttributeMap) Username() (string, bool) {
+func (m *AttributeMap) Username() (string, bool) {
 	v, ok := m.Attribute(UserNameAttr)
 	if ok {
 		v = util.DecodeName(v)
@@ -72,22 +73,22 @@ func (m AttributeMap) Username() (string, bool) {
 }
 
 // FutureExtensions returns the future extensibility attribute from the map.
-func (m AttributeMap) FutureFutureExtensibility() (string, bool) {
+func (m *AttributeMap) FutureFutureExtensibility() (string, bool) {
 	return m.Attribute(FutureExtensibilityAttr)
 }
 
 // RandomSequence returns the random sequence attribute from the map.
-func (m AttributeMap) RandomSequence() (string, bool) {
+func (m *AttributeMap) RandomSequence() (string, bool) {
 	return m.Attribute(RandomSequenceAttr)
 }
 
 // Salt returns the salt attribute from the map.
-func (m AttributeMap) Salt() ([]byte, bool) {
+func (m *AttributeMap) Salt() ([]byte, bool) {
 	return m.DecodeAttribute(SaltAttr)
 }
 
 // IterationCount returns the iteration count attribute from the map.
-func (m AttributeMap) IterationCount() (int, bool) {
+func (m *AttributeMap) IterationCount() (int, bool) {
 	v, ok := m.Attribute(IterationCountAttr)
 	if !ok {
 		return 0, false
@@ -100,87 +101,94 @@ func (m AttributeMap) IterationCount() (int, bool) {
 }
 
 // ClientProof returns the client proof attribute from the map.
-func (m AttributeMap) ClientProof() ([]byte, bool) {
+func (m *AttributeMap) ClientProof() ([]byte, bool) {
 	return m.DecodeAttribute(ClientProofAttr)
 }
 
 // ChannelBindingData returns the channel binding data attribute from the map.
-func (m AttributeMap) ChannelBindingData() (string, bool) {
+func (m *AttributeMap) ChannelBindingData() (string, bool) {
 	return m.Attribute(ChannelBindingDataAttr)
 }
 
 // ServerSignature returns the server signature attribute from the map.
-func (m AttributeMap) ServerSignature() ([]byte, bool) {
+func (m *AttributeMap) ServerSignature() ([]byte, bool) {
 	return m.DecodeAttribute(ServerSignatureAttr)
 }
 
 // Error returns the error attribute from the map.
-func (m AttributeMap) Error() (string, bool) {
+func (m *AttributeMap) Error() (string, bool) {
 	return m.Attribute(ErrorAttr)
 }
 
 // SetAttribute sets an attribute to the map.
-func (m AttributeMap) SetAttribute(name, value string) {
+func (m *AttributeMap) SetAttribute(name, value string) {
 	m.attrs[name] = NewAttribute(name, value)
+	// add name if it is not already in the list
+	for _, key := range m.keys {
+		if key == name {
+			return
+		}
+	}
+	m.keys = append(m.keys, name)
 }
 
 // EncodeAttribute sets a base64 encoded attribute to the map.
-func (m AttributeMap) EncodeAttribute(name string, value []byte) {
+func (m *AttributeMap) EncodeAttribute(name string, value []byte) {
 	m.SetAttribute(name, base64.StdEncoding.EncodeToString(value))
 }
 
 // SetUsername sets the user name attribute to the map.
-func (m AttributeMap) SetUsername(value string) {
+func (m *AttributeMap) SetUsername(value string) {
 	m.SetAttribute(UserNameAttr, util.EncodeName(value))
 }
 
 // SetFutureExtensibility sets the future extensibility attribute to the map.
-func (m AttributeMap) SetFutureExtensibility(value string) {
+func (m *AttributeMap) SetFutureExtensibility(value string) {
 	m.SetAttribute(FutureExtensibilityAttr, value)
 }
 
 // SetRandomSequence sets the random sequence attribute to the map.
-func (m AttributeMap) SetRandomSequence(value string) {
+func (m *AttributeMap) SetRandomSequence(value string) {
 	m.SetAttribute(RandomSequenceAttr, value)
 }
 
 // SetSalt sets the salt attribute to the map.
-func (m AttributeMap) SetSalt(value string) {
+func (m *AttributeMap) SetSalt(value string) {
 	m.SetAttribute(SaltAttr, value)
 }
 
 // SetSaltBytes sets the salt attribute to the map.
-func (m AttributeMap) SetSaltBytes(value []byte) {
+func (m *AttributeMap) SetSaltBytes(value []byte) {
 	m.EncodeAttribute(SaltAttr, value)
 }
 
 // SetIterationCount sets the iteration count attribute to the map.
-func (m AttributeMap) SetIterationCount(value int) {
+func (m *AttributeMap) SetIterationCount(value int) {
 	m.SetAttribute(IterationCountAttr, strconv.Itoa(value))
 }
 
 // SetClientProof sets the client proof attribute to the map.
-func (m AttributeMap) SetClientProof(value []byte) {
+func (m *AttributeMap) SetClientProof(value []byte) {
 	m.EncodeAttribute(ClientProofAttr, value)
 }
 
 // SetChannelBindingData sets the channel binding data attribute to the map.
-func (m AttributeMap) SetChannelBindingData(value string) {
+func (m *AttributeMap) SetChannelBindingData(value string) {
 	m.SetAttribute(ChannelBindingDataAttr, value)
 }
 
 // SetServerSignature sets the server signature attribute to the map.
-func (m AttributeMap) SetServerSignature(value []byte) {
+func (m *AttributeMap) SetServerSignature(value []byte) {
 	m.EncodeAttribute(ServerSignatureAttr, value)
 }
 
 // SetError sets the error attribute to the map.
-func (m AttributeMap) SetError(value string) {
+func (m *AttributeMap) SetError(value string) {
 	m.SetAttribute(ErrorAttr, value)
 }
 
 // Equals returns true if the map is equal to the other map.
-func (m AttributeMap) Equals(other AttributeMap) bool {
+func (m *AttributeMap) Equals(other AttributeMap) bool {
 	if len(m.attrs) != len(other.attrs) {
 		return false
 	}
@@ -197,15 +205,9 @@ func (m AttributeMap) Equals(other AttributeMap) bool {
 }
 
 // String returns the string representation of the map.
-func (m AttributeMap) String() string {
-	keys := []string{}
-	for k := range m.attrs {
-		keys = append(keys, k)
-	}
-	sort.Strings(keys)
-
+func (m *AttributeMap) String() string {
 	attrs := []string{}
-	for _, key := range keys {
+	for _, key := range m.keys {
 		attr := m.attrs[key]
 		attrs = append(attrs, key+"="+attr.Value())
 	}
