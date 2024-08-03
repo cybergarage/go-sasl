dd49fa0f2e6304ca4819e956983a2d45a6a4c6dc
Satoshi Konno <skonno@cybergarage.org>
2024-08-03 14:10:44 +0900

    Add ANONYMOUS plugins

---
+++ sasl/plugins/anonymous/message.go
@@ -0,0 +1,42 @@
+// Copyright (C) 2024 The go-sasl Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package anonymous
+
+import (
+	"fmt"
+)
+
+// Anonymous Simple Authentication and Security Layer (SASL) Mechanism
+// https://www.rfc-editor.org/rfc/rfc4505.html
+
+// Message represents a SASL ANONYMOUS message.
+type Message string
+
+// NewMessage returns a new ANONYMOUS message.
+func NewMessageFrom(v any) (Message, error) {
+	switch v := v.(type) {
+	case Message:
+		return v, nil
+	case string:
+		if 0 < len(v) {
+			return Message(v), nil
+		}
+	case []byte:
+		if 0 < len(v) {
+			return Message(v), nil
+		}
+	}
+	return "", fmt.Errorf("invalid type %T for ANONYMOUS message", v)
+}
---
+++ sasl/plugins/anonymous/server.go
@@ -0,0 +1,90 @@
+// Copyright (C) 2024 The go-sasl Authors. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package anonymous
+
+import (
+	"fmt"
+
+	"github.com/cybergarage/go-sasl/sasl/mech"
+)
+
+// ServerContext represents a PLAIN server context.
+type ServerContext struct {
+	step int
+}
+
+// NewServerContext returns a new PLAIN server context.
+func NewServerContext(opts ...mech.Option) (*ServerContext, error) {
+	ctx := &ServerContext{
+		step: 0,
+	}
+	return ctx, nil
+}
+
+// IsCompleted returns true if the context is completed.
+func (ctx *ServerContext) IsCompleted() bool {
+	return ctx.step == 1
+}
+
+// Step returns the current step number. The step number is incremented by one after each call to Next.
+func (ctx *ServerContext) Step() int {
+	return ctx.step
+}
+
+// Next returns the next response.
+func (ctx *ServerContext) Next(opts ...mech.Parameter) (mech.Response, error) {
+	switch ctx.step {
+	case 0:
+		if len(opts) == 0 {
+			return nil, fmt.Errorf("no message")
+		}
+		_, err := NewMessageFrom(opts[0])
+		if err != nil {
+			return nil, err
+		}
+		ctx.step++
+		return nil, nil
+	}
+
+	return nil, fmt.Errorf("invalid step : %d", ctx.step)
+}
+
+// Dispose disposes the context.
+func (ctx *ServerContext) Dispose() error {
+	return nil
+}
+
+// Server represents a PLAIN mech.
+type Server struct {
+}
+
+func NewServer() mech.Mechanism {
+	return &Server{}
+}
+
+// Name returns the mechanism name.
+func (server *Server) Name() string {
+	return Type
+}
+
+// Type returns the mechanism type.
+func (server *Server) Type() mech.Type {
+	return mech.Server
+}
+
+// Start returns the initial context.
+func (server *Server) Start(opts ...mech.Option) (mech.Context, error) {
+	return NewServerContext(opts...)
+}
