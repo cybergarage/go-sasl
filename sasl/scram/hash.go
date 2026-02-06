308a7239725526dc588b09ebf71664e19cd8ffe0
Satoshi Konno <skonno@cybergarage.org>
2026-02-06 22:53:30 +0900

    style: fix godoclint issues

--- sasl/scram/hash.go
+++ sasl/scram/hash.go
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package scram provides hash functions for SCRAM authentication mechanisms.
+//
 // nolint: gosec
 package scram
 
